056e7bc094603a1732cc1459d817f70fadd0660c
kumarapusrivarsha <134908628+kumarapusrivarsha@users.noreply.github.com>
2026-07-19 12:21:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+// Display message when page loads
+
+window.onload = function(){
+
+    console.log("Welcome to Accio Jobs Blog Page");
+
+};
+
+
+
+// Add click interaction to article links
+
+const articleLinks = document.querySelectorAll("article h2 a");
+
+
+articleLinks.forEach(function(link){
+
+    link.addEventListener("click",function(){
+
+        alert("Opening Accio Jobs Article");
+
+    });
+
+});
